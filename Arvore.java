e2d2dbb246e5e0604e6a0c98b70c5dd569d037a6
Rafa_Note <rafinhapsampaio@hotmail.com>
2024-03-25 12:37:06 -0300

    Correção de erros bestas

--- Arvore.java
+++ Arvore.java
@@ -10,20 +10,21 @@ class Node {  //classe do Node
     }
 }
 
- public static void main(String[] args) {
+public class Arvore {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.println("Menu:");
-            System.out.println("1) Mostrar nível de um determinado nó");
-            System.out.println("2) nível da árvore");
-            System.out.println("3) Mostrar nós e suas profundidades");
-            System.out.println("4) Mostrar profundidade da árvore");
-            System.out.println("5) Mostrar altura de cada nó");
-            System.out.println("6) Mostrar altura da árvore");
-            System.out.println("7) Impressão da árvore com identação");
-            System.out.println("8) Inserir um valor na árvore");
-            System.out.println("0) Sair");
+            System.out.println("1 Mostrar nível de um determinado nó");
+            System.out.println("2 Nível da árvore");
+            System.out.println("3 Mostrar nós e suas profundidades");
+            System.out.println("4 Mostrar profundidade da árvore");
+            System.out.println("5 Mostrar altura de cada nó");
+            System.out.println("6 Mostrar altura da árvore");
+            System.out.println("7 Impressão da árvore com identação");
+            System.out.println("8 Inserir um valor na árvore");
+            System.out.println("0 Sair");
 
             System.out.print("Escolha uma opção: ");
             char option = scanner.next().charAt(0);
@@ -33,4 +34,5 @@ class Node {  //classe do Node
                 //Colocar opções aqui usando case
             }
         }
- }
+    }
+}
